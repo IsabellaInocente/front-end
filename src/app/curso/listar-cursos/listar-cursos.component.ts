12d79f2be856dad57e5ed13152cc0a5ec6f061b1
IsabellaInocente <inocenteisabella@gmail.com>
2023-11-07 11:42:06 -0300

    Primeiro deploy

---
+++ src/app/curso/listar-cursos/listar-cursos.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Icursos } from '../Service/icursos';
+import { CursosService } from '../Service/cursos.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormControl, FormGroup} from '@angular/forms';
+
+@Component({
+  selector: 'app-listar-cursos',
+  templateUrl: './listar-cursos.component.html',
+  styleUrls: ['./listar-cursos.component.scss']
+})
+export class ListarCursosComponent implements OnInit {
+  cursos: Icursos[]=[];
+
+  form = new FormGroup({
+    id: new FormControl(),
+    nome: new FormControl(''),
+  })
+
+  constructor(
+    private service: CursosService,
+    private router: Router,
+    private route: ActivatedRoute){}
+
+    ngOnInit(): void {
+        this.Listar();
+    }
+    Listar(){
+      this.service.listar().subscribe(dados => this.cursos = dados);
+    }
+    Editar(id: number){
+      this.router.navigate(['editar', id], {relativeTo: this.route});
+    }
+    Excluir(id: number){
+      this.service.excluir(id).subscribe(
+        success =>{
+          alert("Curso exclundo com sucesso!")
+          this.service.listar().subscribe(dados => this.cursos = dados);
+        },
+        Error => alert("Erro ao excluir o curso")
+      );
+    }
+}
